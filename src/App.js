592381771def86bbde8b07b41784cc8ac5c36725
Rahul <bhaleraor17@gmail.com>
2024-03-28 16:36:34 +0530

    countries search implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const fetchCountry = async () => {
+    try {
+      let response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+      setFilteredCountries(response.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    fetchCountry();
+
+  }, [])
+
+  const handleChange = (e) => {
+    let search = e.target.value.toLowerCase();
+    setSearchTerm(search);
+    const filtered = countries.filter(country =>
+      country.name.common.toLowerCase().includes(search)
+    )
+    setFilteredCountries(filtered);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <input className="input-country" placeholder="Search for the countries" value={searchTerm} type="text" onChange={handleChange} />
+      <div className="container">
+        {filteredCountries.map((country) => (<div className="countryCard"><img className="imgStyle" src={country.flags.png} alt={country.name.common} />
+          <h3>{country.name.common}</h3></div>))}
+
+      </div>
     </div>
   );
 }
